b38db537f031efc82ed4c1a55ec1b8d851e75049
PK <15170689+pawanline@users.noreply.github.com>
2024-04-01 22:58:37 +0530

    SRG education changes

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -6,8 +6,8 @@ const Head = () => {
       <section className='head'>
         <div className='container flexSB'>
           <div className='logo'>
-            <h1>ACADEMIA</h1>
-            <span>ONLINE EDUCATION & LEARNING</span>
+            <h1>SRG Computer</h1>
+            <span>Training Institue</span>
           </div>
 
           <div className='social'>
