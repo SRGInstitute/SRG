c59149dec2f3ce6f45e824e21934ccf09e6b3f84
SRG Computer Institute <163426495+SRGInstitute@users.noreply.github.com>
2024-07-20 23:21:13 +0530

    social link updated

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -11,7 +11,7 @@ const Head = () => {
           </div>
 
           <div className='social'>
-            <i className='fab fa-facebook-f icon'></i>
+            <Link to={{ pathname: "https://www.facebook.com/SRGComputerInstitute" }} target="_blank"><i className='fab fa-facebook-f icon'></i></Link>
             <i className='fab fa-instagram icon'></i>
             <i className='fab fa-twitter icon'></i>
             <i className='fab fa-youtube icon'></i>
