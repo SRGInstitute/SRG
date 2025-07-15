af7cdbfebaa0c7e6d8e5fc63e9c1459a5a26233b
SRG Computer Institute <163426495+SRGInstitute@users.noreply.github.com>
2025-07-15 20:11:05 +0530

    Update PrivacyContainter.jsx

--- src/components/legal/PrivacyContainter.jsx
+++ src/components/legal/PrivacyContainter.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Back from '../common/back/Back';
 import PrivacyCard from '../legal/privacy-policy'; // Separate component for the content
-import './PrivacyContent.css';
+import './PrivacyCard.css';
 const PrivacyPolicy = () => {
   return (
     <>
